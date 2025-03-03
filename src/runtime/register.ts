bdacf77600e1f9af13e5c77d689c5da609d4589e
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-03-03 16:29:26 -0500

    feat: lazily create sprite root
    This allows the containerSelector to not exist immediately.

--- src/runtime/register.ts
+++ src/runtime/register.ts
@@ -18,62 +18,67 @@ function createRegisterFunction(): RegisterFunction {
     return () => ssrFallback;
   }
 
-  // This must be the only reference to document body, or else the Vite plugin will fail to
-  // transform this module correctly.
-  const container = document.body;
-  const containerDoc = container.ownerDocument;
-  const containerRoot = container.shadowRoot ?? containerDoc;
-  const containerBody =
-    container.shadowRoot?.querySelector('body') ?? container;
+  let root: SVGSVGElement;
+  let containerRoot: Document | ShadowRoot;
+  let idSet: Set<string>;
 
-  const idSet: Set<string> =
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ((container as any)._SVG_SPRITE_IDS_ ||= new Set());
+  const setup = () => {
+    // This must be the only reference to document body, or else the Vite plugin will fail to
+    // transform this module correctly.
+    const container = document.body;
+    const containerDoc = container.ownerDocument;
+    const containerBody =
+      container.shadowRoot?.querySelector('body') ?? container;
 
-  const root = containerDoc.createElementNS(
-    'http://www.w3.org/2000/svg',
-    'svg',
-  );
-  root.style.position = 'absolute';
-  root.style.width = '0';
-  root.style.height = '0';
-  root.style.overflow = 'hidden';
-  root.ariaHidden = 'true';
+    containerRoot = container.shadowRoot ?? containerDoc;
+    idSet =
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (container as any)._SVG_SPRITE_IDS_ ||= new Set();
 
-  // DO NOT SET THIS
-  // root.style.visibility = 'hidden';
+    root = containerDoc.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    root.style.position = 'absolute';
+    root.style.width = '0';
+    root.style.height = '0';
+    root.style.overflow = 'hidden';
+    root.ariaHidden = 'true';
 
-  function insertRoot() {
-    containerBody.insertBefore(root, containerBody.firstChild);
-  }
+    // DO NOT SET THIS
+    // root.style.visibility = 'hidden';
 
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', insertRoot);
-  } else {
-    insertRoot();
-  }
-
-  return function registerSymbol(symbol: string, id: string) {
-    if (idSet.has(id) || containerRoot.getElementById(id)) {
-      console.warn(
-        `Icon #${id} was repeatedly registered. It must be globally unique.`,
-      );
+    function insertRoot() {
+      containerBody.insertBefore(root, containerBody.firstChild);
     }
 
-    idSet.add(id);
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', insertRoot);
+    } else {
+      insertRoot();
+    }
+  };
 
+  return function registerSymbol(symbol: string, id: string) {
     let symbolElement: ChildNode | null = null;
 
     return {
       mount() {
         if (!symbolElement) {
+          if (!root) setup();
           root.insertAdjacentHTML('beforeend', symbol);
           symbolElement = root.lastChild;
+
+          if (idSet.has(id) || containerRoot.getElementById(id)) {
+            console.warn(
+              `Icon #${id} was repeatedly registered. It must be globally unique.`,
+            );
+          }
+          idSet.add(id);
         }
       },
       unmount() {
-        idSet.delete(id);
-        symbolElement?.remove();
+        if (symbolElement) {
+          symbolElement.remove();
+          idSet.delete(id);
+        }
       },
     };
   };
